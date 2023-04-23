017020823f3f76caa686f5b13be2e6e943b6cb71
unknown <tangyuang01@gmail.com>
2023-04-23 19:13:05 -0400

    add sortby

--- pa04/routes/store.js
+++ pa04/routes/store.js
@@ -4,37 +4,35 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //for reading values
 const router = express.Router();
-const Transaction = require('../models/Transaction')
+const Transaction = require('../models/Transaction');
 
-
-/*
-this is a very simple server which maintains a key/value
-store using an object where the keys and values are lists of strings
-
-*/
-
-// get the value associated to the key
+// get the value associated with the key
 router.get('/transactions/',
   isLoggedIn,
   async (req, res, next) => {
-    res.locals.transactions = await Transaction.find({ userId: req.user._id })
+    const transactions = await Transaction.find({ userId: req.user._id });
+    console.log('Fetched transactions:', transactions); // Log the fetched transactions
+    res.locals.transactions = transactions;
     res.render('transactions');
   });
 
-/* add the value in the body to the list associated to the key */
+/* add the value in the body to the list associated with the key */
 router.post('/transactions',
   isLoggedIn,
   async (req, res, next) => {
     const transaction = new Transaction(
       {
+        userId: req.user._id,
         description: req.body.description,
         category: req.body.category,
         amount: req.body.amount,
         date: new Date(req.body.date).toDateString()
-      })
-    await transaction.save();
-    res.redirect('/transactions'); // Redirect to the transactions page
+      });
+    const savedTransaction = await transaction.save();
+    console.log('Saved transaction:', savedTransaction); // Log the saved transaction
+    res.redirect('/transactions');
   });
+
 
 router.get('/transactions/delete/:itemId',
   isLoggedIn,
@@ -44,11 +42,42 @@ router.get('/transactions/delete/:itemId',
     res.redirect('/transactions')
   });
 
-router.get('/transactions/sortBy=date'),
+// Sort transactions by date
+// Aaron Tang
+router.get('/transactions/sortBy=date',
   isLoggedIn,
   async (req, res, next) => {
     res.locals.transactions = await Transaction.find({ userId: req.user._id }).sort({ date: 1 })
     res.render('transactions');
-  }
+  });
+
+// Sort transactions by amount
+// Aaron Tang
+router.get('/transactions/sortBy=amount',
+  isLoggedIn,
+  async (req, res, next) => {
+    res.locals.transactions = await Transaction.find({ userId: req.user._id }).sort({ amount: 1 });
+    res.render('transactions');
+  });
+
+// Sort transactions by category
+// Aaron Tang
+router.get('/transactions/sortBy=category',
+  isLoggedIn,
+  async (req, res, next) => {
+    res.locals.transactions = await Transaction.find({ userId: req.user._id }).sort({ category: 1 });
+    res.render('transactions');
+  });
+
+
+// Sort transactions by description
+// Aaron Tang
+router.get('/transactions/sortBy=description',
+  isLoggedIn,
+  async (req, res, next) => {
+    res.locals.transactions = await Transaction.find({ userId: req.user._id }).sort({ description: 1 });
+    res.render('transactions');
+  });
 
 module.exports = router;
+
