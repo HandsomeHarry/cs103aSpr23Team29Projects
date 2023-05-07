0b24cb5cb61e72102a6e8cfa94403ebab0c4baa0
HandsomeHarry <harryyu2002@gmail.com>
2023-05-06 22:09:06 -0400

    CA02 works

--- ca02/firstapp/routes/gpt.js
+++ ca02/firstapp/routes/gpt.js
@@ -7,8 +7,8 @@ const ToDoItem = require("../models/ToDoItem");
 const User = require("../models/User");
 const axios = require("axios");
 
-const harryPrompt = "";
-const harryGPTPrompt = "";
+const harryPrompt = "Enter a field that you would like to analyze: ";
+const harryGPTPrompt = "Analyze the following field for me, include various points of view, future prospect and profitability: \n";
 
 const zaredPrompt = "Enter something to translate to Python: ";
 const zaredGPTPrompt = "Translate this to Python: \n";
@@ -24,41 +24,26 @@ isLoggedIn = (req, res, next) => {
   }
 };
 
-require("dotenv").config();
-
-const apiKey = process.env.APIKEY;
+//require("dotenv").config();
+//const apiKey = process.env.APIKEY;
 
 class GPT {
-  constructor(apiKey) {
-    this.apiKey = apiKey;
-    this.modelEngine = "text-davinci-003";
-    this.apiUrl =
-      "https://api.openai.com/v1/engines/" + this.modelEngine + "/completions";
+  constructor() {
+    //this.apiKey = apiKey;
+    //this.modelEngine = "text-davinci-003";
+    //this.apiUrl = "https://api.openai.com/v1/engines/" + this.modelEngine + "/completions";
   }
 
   async getResponse(prompt) {
-    const response = await axios.post(
-      this.apiUrl,
-      {
-        prompt: prompt,
-        max_tokens: 1024,
-        n: 1,
-        stop: null,
-        temperature: 0.8,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${this.apiKey}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai', 
+    {prompt: prompt});
 
-    return response.data.choices[0].text;
+    console.log(response.data.choices[0].message.content);
+    return response.data.choices[0].message.content;
   }
 }
 
-const gpt = new GPT(apiKey);
+const gpt = new GPT();
 
 router.get("/gpt", isLoggedIn, async (req, res, next) => {
   const prompt = req.query.prompt;
@@ -76,15 +61,17 @@ router.get("/gpt", isLoggedIn, async (req, res, next) => {
 router.post("/gpt", isLoggedIn, async (req, res, next) => {
   const prompt = req.body.prompt;
   const input = req.body.input;
+  response = undefined;
   console.log(prompt + " " + input);
   if (prompt == zaredPrompt) {
-    const response = await gpt.getResponse(zaredGPTPrompt + input);
+    response = await gpt.getResponse(zaredGPTPrompt + input);
   } else if (prompt == harryPrompt) {
-    const response = await gpt.getResponse(harryGPTPrompt + input);
+    response = await gpt.getResponse(harryGPTPrompt + input);
   } else if (prompt == aaronPrompt) {
-    const response = await gpt.getResponse(aaronGPTPrompt + input);
+    response = await gpt.getResponse(aaronGPTPrompt + input);
   } else {
   }
+  console.log(response)
   res.render("gpt", { prompt, input, response });
 });
 
